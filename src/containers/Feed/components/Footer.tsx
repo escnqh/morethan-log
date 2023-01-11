f5a8a38e13017d694391b7488a029c255e8e93d8
Peelson <github@peelson.cn>
2023-01-11 17:56:25 +0800

    [Feature] 去掉页脚跳转

--- src/containers/Feed/components/Footer.tsx
+++ src/containers/Feed/components/Footer.tsx
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-01-11 14:20:45
+ * @LastEditors: Peelson github@peelson.cn
+ * @LastEditTime: 2023-01-11 17:56:10
+ * @FilePath: /morethan-log/src/containers/Feed/components/Footer.tsx
+ */
 import CONFIG from '@/morethan-log.config'
 import Link from 'next/link'
 import React from 'react'
@@ -14,7 +20,7 @@ const Footer: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
       <a
-        href={'https://github.com/morethanmin/morethan-log'}
+        href={''}
         target="_blank"
         className="text-gray-500 text-sm mt-3"
         rel="noreferrer"
